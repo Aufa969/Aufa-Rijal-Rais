a991e684d4d3caa1ae5088cc852e9b89b7e84c31
Aufa969 <aufarijal000@gmail.com>
2025-01-15 11:13:23 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,18 @@
+let namaDepan = "Aufa";
+let namaTengah = "Rijal";
+let namaBelakang = "Rais";
+let alamat = "Sayung";
+let usia = "15";
+let ttl = "Semarang,26 Juli 2009";
+let gender = "Laki-laki";
+let agama = "Islam";
+let email = "aufarijal000@gmail.com";
+let berat = "56";
+let tinggi = "160";
+let hobi = "main game";
+let cita = "Pengusaha";
+let pekerjaan = "pelajar";
+let sekolah = "SMKN 1 SAYUNG";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah" + namaDepan + " " + namaTengah + " " + namaBelakang + "saya tingggal di" + alamat + "pada tahun 2024 ini usia saya adalah" + usia + "saya lahir di" + ttl + "jenis kelamin saya adalah" + gender + "saya beragama" + agama + "alamat email saya adalah" + email + "saya memiliki berat badan" + berat + "kg dengan tinggi" + tinggi + "cm" + "saya sangat senang" + hobi + "karena saya memiliki cita-cita" + cita + "saat ini saya adalah seorang" + pekerjaan + "yang bersekolah di" + sekolah + "pada jurusan" + jurusan);
